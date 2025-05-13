cb09127482a5b5254ff6f1fac74d3a47a6822d3a
marilia pereira <carlos.sousa115@etec.sp.gov.br>
2025-05-12 22:33:25 -0300

    Banco Madeira

---
+++ arquivoBD.sql
@@ -0,0 +1,11 @@
+
+Create database Loja_Madeira;
+use Loja_Madeira;
+Create table Madeiras;
+(Cod_Madeira int primary key,
+Tipo_Madeira varchar(50),
+idade_brinq Tinyint,
+Qtd_Chapa int,
+Tam_Chapa,
+Val_Madeira decimal(10,2));
+Insert into Madeira values
