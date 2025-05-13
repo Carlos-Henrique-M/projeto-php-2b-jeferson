672e6f6ab0a5d0e3124611113378302610881656
marilia pereira <carlos.sousa115@etec.sp.gov.br>
2025-05-12 22:27:37 -0300

    Classe de Cadeira

---
+++ Modelos.php
@@ -0,0 +1,14 @@
+<?php 
+public class Cadeira 
+
+Cadeira simples
+
+Assento: 450 x 450 mm (1 peça)
+
+Encosto: 450 x 400 mm (1 peça)
+
+Pés: 40 x 40 x 450 mm (4 peças)
+
+Travessas: 40 x 40 x 400 mm (4 peças)
+
+?> 
